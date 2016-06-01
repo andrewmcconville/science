a35cd6db18f625cf815c50821e7b40468d6f60b9
Andrew McConville <amcconville@acculynx.com>
2016-05-31 19:39:23 -0500

    appFilters not found on heroku

--- public/js/appFIlters.js
+++ public/js/appFIlters.js
@@ -1,15 +1,17 @@
-app.filter('getYear', function(){
-	return function(UTC){
-		return new Date(UTC).getUTCFullYear();
-	}
-});
+// app.filter('getYear', function(){
+// 	return function(UTC){
+// 		return new Date(UTC).getUTCFullYear();
+// 	}
+// });
 
-app.filter('getDate', ['$filter', function($filter){
-	return function(UTC){
-		if(UTC < 0){
-			return Math.abs(new Date(UTC).getUTCFullYear());
-		} else {
-			return $filter('date')(new Date(UTC), 'y');
-		}
-	}
-}]);
+// app.filter('getDate', ['$filter', function($filter){
+// 	return function(UTC){
+// 		if(UTC < 0){
+// 			return Math.abs(new Date(UTC).getUTCFullYear());
+// 		} else {
+// 			return $filter('date')(new Date(UTC), 'y');
+// 		}
+// 	}
+// }]);
+
+console.log('filters');
