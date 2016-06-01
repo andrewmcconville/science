ab2c034f090d1645b7f5e98ae720d889c21a31d5
Andrew McConville <amcconville@acculynx.com>
2016-05-31 20:37:51 -0500

    moves git readme

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var compress = require('compression');
 var express = require('express');
 var app = express();
 
+// activate gzip
 app.use(compress());  
 
 // set the port of our application
@@ -11,14 +12,12 @@ var port = process.env.PORT || 8080;
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
-// app.use(function(req, res) {
-//     res.sendFile(__dirname + '/public/index.html');
-// });
-
+// all URLs go to index so angular can run first
 app.get('*', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
 });
 
+// terminal message
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
