2d73fdc4bfc32c8de16a94bd5951d2287c2cdb77
Andrew McConville <amcconville@acculynx.com>
2016-05-31 19:43:03 -0500

    the i in appFilters was capitalized

--- public/js/app.js
+++ public/js/app.js
@@ -59,19 +59,3 @@ app.config(['$urlRouterProvider', '$stateProvider', '$locationProvider', '$compi
 	$locationProvider.html5Mode(true);
 	$locationProvider.hashPrefix('!');
 }]);
-
-app.filter('getYear', function(){
-	return function(UTC){
-		return new Date(UTC).getUTCFullYear();
-	}
-});
-
-app.filter('getDate', ['$filter', function($filter){
-	return function(UTC){
-		if(UTC < 0){
-			return Math.abs(new Date(UTC).getUTCFullYear());
-		} else {
-			return $filter('date')(new Date(UTC), 'y');
-		}
-	}
-}]);
--- public/js/appFIlters.js
+++ public/js/appFIlters.js
@@ -1,17 +1,15 @@
-// app.filter('getYear', function(){
-// 	return function(UTC){
-// 		return new Date(UTC).getUTCFullYear();
-// 	}
-// });
+app.filter('getYear', function(){
+	return function(UTC){
+		return new Date(UTC).getUTCFullYear();
+	}
+});
 
-// app.filter('getDate', ['$filter', function($filter){
-// 	return function(UTC){
-// 		if(UTC < 0){
-// 			return Math.abs(new Date(UTC).getUTCFullYear());
-// 		} else {
-// 			return $filter('date')(new Date(UTC), 'y');
-// 		}
-// 	}
-// }]);
-
-console.log('filters');
+app.filter('getDate', ['$filter', function($filter){
+	return function(UTC){
+		if(UTC < 0){
+			return Math.abs(new Date(UTC).getUTCFullYear());
+		} else {
+			return $filter('date')(new Date(UTC), 'y');
+		}
+	}
+}]);
