2473ba712ac634691202c1413a96f274242fa08d
Andrew McConville <amcconville@acculynx.com>
2016-06-04 18:03:45 -0500

    removes last config option

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 // activate gzip
 app.use(compress());
 
-app.use(require('prerender-node')).set('prerenderServiceUrl', '<new url>'));
+app.use(require('prerender-node'));
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
