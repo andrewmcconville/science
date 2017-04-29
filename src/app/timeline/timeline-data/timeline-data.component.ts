b1ae6b047ad0e5442df06909e734c57a2fb2277c
Andrew McConville <andrew@andrewmcconville.com>
2017-04-29 18:45:24 -0500

    stubbing timeline data

---
+++ src/app/timeline/timeline-data/timeline-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sciti-timeline-data',
+    templateUrl: './timeline-data.component.html',
+    styleUrls: ['./timeline-data.component.scss']
+})
+export class TimelineDataComponent {
+    constructor() { }
+}
